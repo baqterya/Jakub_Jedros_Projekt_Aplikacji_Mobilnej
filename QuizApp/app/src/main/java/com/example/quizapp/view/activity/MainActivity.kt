fee6c39b4fa9bd46732ab20f3f24a1fe8788d990
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2021-04-07 13:36:13 +0200

    Przebudowa interfejsu, usunięcie menu głównego i zastąpienie go przez navigation bar

--- QuizApp/app/src/main/java/com/example/quizapp/view/activity/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/view/activity/MainActivity.kt
@@ -2,16 +2,46 @@ package com.example.quizapp.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.quizapp.R
-import com.example.quizapp.view.fragment.MainMenuFragment
+import com.example.quizapp.view.fragment.LanguageListFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.main_menu_container, MainMenuFragment())
+        fragmentTransaction.replace(R.id.recyclerViewContainer, LanguageListFragment())
         fragmentTransaction.commit()
+
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_question_sets -> Toast.makeText(applicationContext, "question sets", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
